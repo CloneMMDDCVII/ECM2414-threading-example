158d23218d5f5298c24f6b3fd5a1f1ae7b233ba4
pierre <dumboyadvance@gmail.com>
2020-10-07 15:32:57 +0100

    just random commenting

--- src/Job.java
+++ src/Job.java
@@ -17,20 +17,5 @@ private int jobNumber;
         System.out.println("Job: " + jobNumber + " is ending in thread : "
                 + Thread.currentThread().getName());
         }
-    /*Job (int jobNumber) {
-        this.jobNumber = jobNumber;
-    }
-
-    public void run () {
-        // Undertake required work, here we will emulate it by sleeping for a period
-        System.out.println ("Job: " + jobNumber + " is being processed by thread : "
-                + Thread.currentThread ().getName());
-        try {
-            Thread.sleep((int)(1000));
-        } catch (InterruptedException e) {
-            // no catching as example should not experience interruptions
-        }
-        System.out.println("Job: " + jobNumber + " is ending in thread : "
-                + Thread.currentThread().getName());
-    }*/
+//This is all about dev
 }
